2b0dab721cb709c2c8a95519ce5da49b27f407b9
sunny7319 <sunyoung0509@naver.com>
2024-05-22 17:54:28 +0900

    server.py

--- server.py
+++ server.py
@@ -1,23 +1,77 @@
 from flask import Flask
 import random
+from flask import request
 
 app = Flask(__name__)
 
 
+topics = [
+    {'id': 1, 'title': 'HTML', 'body': 'HTML is ...'},
+    {'id': 2, 'title': 'CSS', 'body': 'CSS is ...'},
+    {'id': 3, 'title': 'JavaScript', 'body': 'JavaScript is ...'}
+]
+
+
+def template(contents, content):
+    return f'''<!doctype html>
+    <html>
+        <body>
+            <h1><a href="/">WEB</a></h1>
+            <ol>
+                {contents}
+            </ol>
+            {content}
+            <ul>
+                <li><a href="/create/">create</a></li>
+            </ul>
+        </body>
+    </html>
+    '''
+
+
+def getContents():
+    liTags = ''
+    for topic in topics:
+        liTags = liTags + f'<li><a href="/read/{topic["id"]}/">{topic["title"]}</a></li>'
+    
+    return liTags
+
+
 @app.route('/')
 def index():
-    return 'Welcome'
+    return template(getContents(), '<h2>Welcome</h2>Hello, Web')
 
 
-@app.route('/create/')
-def create():
-    return 'Create'
-
-
-@app.route('/read/<id>/')
+@app.route('/read/<int:id>/')
 def read(id):
-    print(id)
-    return 'Read' + id
+    title = ''
+    body = ''
+    for topic in topics:
+        if id == topic['id']:
+            title = topic['title']
+            body = topic['body']
+            break
+
+    return template(getContents(), f'<h2>{title}</h2>{body}')
+
+
+@app.route('/create/', methods=['GET', 'POST'])
+def create():
+    if request.method =='GET':
+        content = '''
+            <form action="/create/" method="POST">
+                <p><input type="text" name="title" placeholder="title"></p>
+                <p><textarea name="body" placeholder="body"></textarea></p>
+                <p><input type="submit" value="create"></p>
+            </form>
+        '''
+        return template(getContents(), content)
+    elif request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        return title + ',' + body
+
+
 
 
 app.run(port=5001, debug=True)
