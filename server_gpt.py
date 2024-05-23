8c0779ab36cf207de6e1770a29ea9ead07fca70a
pch229 <splashch@naver.com>
2024-05-23 13:34:02 +0900

    로그찍히는코드추가

--- server_gpt.py
+++ server_gpt.py
@@ -1,4 +1,8 @@
 from flask import Flask, request, redirect
+from flask import Flask, request, jsonify
+import logging
+from logging.handlers import RotatingFileHandler
+import json
 
 app = Flask(__name__)
 
@@ -119,4 +123,44 @@ def delete(id):
             break
     return redirect('/')
 
-app.run(debug=True)
+# JSON 로그 포맷터 설정
+class JSONFormatter(logging.Formatter):
+    def format(self, record):
+        log_record = {
+            'time': self.formatTime(record, self.datefmt),
+            'level': record.levelname,
+            'message': record.getMessage(),
+            'path': record.pathname,
+            'func': record.funcName,
+            'line': record.lineno,
+            'request': request.method + " " + request.url if request else None,
+            'ip': request.remote_addr if request else None,
+        }
+        return json.dumps(log_record)
+
+# RotatingFileHandler 설정
+handler = RotatingFileHandler('log.json', maxBytes=10000, backupCount=1)
+handler.setLevel(logging.INFO)
+
+# JSON 포맷터 설정
+formatter = JSONFormatter()
+handler.setFormatter(formatter)
+
+# Flask 애플리케이션에 핸들러 추가
+app.logger.addHandler(handler)
+app.logger.setLevel(logging.INFO)
+
+# @app.route('/test', methods=['POST'])
+# def hello_world():
+#     app.logger.info('Home page accessed')
+#     return 'Hello, World!'
+
+@app.route('/test', methods=['POST'])
+def test():
+    data = request.get_json()
+    app.logger.info('Test endpoint accessed with data: %s', data)
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+# app.run(debug=True)
