05ec2261cfb6e425ab42a5b843bb909f8d149b1b
sunny7319 <sunyoung0509@naver.com>
2024-05-23 13:15:35 +0900

    templates

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Hands_MediaPipe_project
+
+your_project/
+│
+├── app.py
+└── templates/
+    └── index.html
+└── static/
+    └── style.css
--- server_gpt.py
+++ server_gpt.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, render_template
 
 app = Flask(__name__)
 
@@ -9,68 +9,22 @@ topics = [
     {'id': 3, 'title': 'JavaScript', 'body': 'JavaScript is ...'}
 ]
 
-def template(contents, content, id=None):
-    contextUI = ''
-    if id != None:
-        contextUI = f'''
-            <li><a href="/update/{id}/">update</a><li>
-            <li>
-                <form action="/delete/{id}/" method="POST">
-                    <input type="submit" value="delete">
-                </form>
-            </li>
-        '''
-    return f'''<!doctype html>
-    <html>
-        <body>
-            <h1><a href="/">WEB</a></h1>
-            <ol>
-                {contents}
-            </ol>
-            {content}
-            <ul>
-                <li><a href="/create/">create</a></li>
-                {contextUI}
-            </ul>
-        </body>
-    </html>
-    '''
-
 def getContents():
-    liTags = ''
-    for topic in topics:
-        liTags = liTags + f'<li><a href="/read/{topic["id"]}/">{topic["title"]}</a></li>'
-    
-    return liTags
+    return topics
 
 @app.route('/')
 def index():
-    return template(getContents(), '<h2>Welcome</h2>Hello, Web')
+    return render_template('index.html', topics=getContents())
 
 @app.route('/read/<int:id>/')
 def read(id):
-    title = ''
-    body = ''
-    for topic in topics:
-        if id == topic['id']:
-            title = topic['title']
-            body = topic['body']
-            break
-
-    return template(getContents(), f'<h2>{title}</h2>{body}', id)
+    topic = next((topic for topic in topics if topic['id'] == id), None)
+    return render_template('read.html', topic=topic, topics=getContents())
 
 @app.route('/create/', methods=['GET', 'POST'])
 def create():
-    if request.method =='GET':
-        content = '''
-            <form action="/create/" method="POST">
-                <p><input type="text" name="title" placeholder="title"></p>
-                <p><textarea name="body" placeholder="body"></textarea></p>
-                <p><input type="submit" value="create"></p>
-            </form>
-        '''
-        return template(getContents(), content)
-    
+    if request.method == 'GET':
+        return render_template('create.html', topics=getContents())
     elif request.method == 'POST':
         global nextId
         title = request.form['title']
@@ -83,23 +37,9 @@ def create():
 
 @app.route('/update/<int:id>/', methods=['GET', 'POST'])
 def update(id):
-    if request.method =='GET':
-        title = ''
-        body = ''
-        for topic in topics:
-            if id == topic['id']:
-                title = topic['title']
-                body = topic['body']
-                break
-        content = f'''
-            <form action="/update/{id}/" method="POST">
-                <p><input type="text" name="title" placeholder="title" value="{title}"></p>
-                <p><textarea name="body" placeholder="body">{body}</textarea></p>
-                <p><input type="submit" value="update"></p>
-            </form>
-        '''
-        return template(getContents(), content)
-    
+    topic = next((topic for topic in topics if topic['id'] == id), None)
+    if request.method == 'GET':
+        return render_template('update.html', topic=topic, topics=getContents())
     elif request.method == 'POST':
         title = request.form['title']
         body = request.form['body']
@@ -113,10 +53,8 @@ def update(id):
 
 @app.route('/delete/<int:id>/', methods=['POST'])
 def delete(id):
-    for topic in topics:
-        if id == topic['id']:
-            topics.remove(topic)
-            break
+    global topics
+    topics = [topic for topic in topics if topic['id'] != id]
     return redirect('/')
 
-app.run(debug=True)
+app.run(port=5001, debug=True)
