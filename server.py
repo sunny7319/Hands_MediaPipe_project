fda0dde0f7eaa4651bc801cec0e19b3bcb21218c
sunny7319 <sunyoung0509@naver.com>
2024-05-27 12:40:28 +0900

    게임3개 연결 및 디자인

--- server.py
+++ server.py
@@ -13,17 +13,17 @@ def index():
 @app.route('/game/<game_name>')
 def game(game_name):
     game_data = {
-        'game1': {
+        '그림자 놀이': {
             'title': '그림자 놀이',
             'image': 'game1.png',
             'description': '손으로 동물이나 모양을 표현하는 놀이입니다'
         },
-        'game2': {
+        '두더지 잡기': {
             'title': '두더지 잡기',
             'image': 'game1.png',
             'description': '주어진 문제에 맞는 단어를 가지고 있는 두더지를 잡는 게임입니다'
         },
-        'game3': {
+        '산성비': {
             'title': '산성비',
             'image': 'game1.png',
             'description': '제시된 알파벳과 같은 글자를 고르는 게임입니다.'
