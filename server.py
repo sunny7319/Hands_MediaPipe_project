faec50c3aeed6f93e3315ec4e0a2e5bc368bfb2b
sunny7319 <sunyoung0509@naver.com>
2024-05-28 10:30:38 +0900

    ..

--- server.py
+++ server.py
@@ -10,6 +10,7 @@ mp_hands = mp.solutions.hands
 hands = mp_hands.Hands()
 mp_draw = mp.solutions.drawing_utils
 
+toggle = 0
 def generate_frames():
     cap = cv2.VideoCapture(0)
     while True:
@@ -23,31 +24,43 @@ def generate_frames():
             if result.multi_hand_landmarks:
                 for hand_landmarks in result.multi_hand_landmarks:
                     mp_draw.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+                    toggle = 1
             
             ret, buffer = cv2.imencode('.jpg', frame)
             frame = buffer.tobytes()
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
             
-# cap = cv2.VideoCapture(0)
-# cap.set(cv2.CAP_PROP_FRAME_WIDTH, 4000)  # 캠 최대 해상도가 1280x720 / ToDo 1 : cv2 함수로 늘려야 함
-# cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 4000)
+            return toggle
 
-# def generate_frames():
+# def generate_frames():    
+#     cap = cv2.VideoCapture(0)
+#     # cap.set(cv2.CAP_PROP_FRAME_WIDTH, 4000)  # 캠 최대 해상도가 1280x720 / ToDo 1 : cv2 함수로 늘려야 함
+#     # cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 4000)  # 가로세로 기본값 640x480 ??
+
+#     toggle = 0
+
 #     if cap.isOpened():
-#         print('width: {}, height : {}'.format(cap.get(3), cap.get(4)))  # 화면크기 1280x720를 프린트 -> DB에 저장해보세요
-#                                                                         # cap.get(3) = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+#         w = cap.get(3)
+#         h = cap.get(4)
+#         print('width: {}, height : {}'.format(w, h))  # 화면크기 ???x???를 프린트
+#                                                     # cap.get(3) = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
 #     while True:
 #         ret, fram = cap.read()
         
 #         if ret:
-
-#             cv2.imshow('cam', cv2.flip(fram, 1))
+#             fram = fram[ : , int(w*1/8):int(w*7/8)]
+#             ratio = 1080/h*(4/3.)  # 전체화면 full-HD 1920x1080 기준, 현재 컴 2560x1440
+#             fram = cv2.resize(cv2.flip(fram, 1), None, fx=ratio, fy=ratio,
+#                             interpolation=cv2.INTER_LINEAR)
+#             cv2.imshow('game', fram)
 
 #             key = cv2.waitKey(10)
 
 #             if key == ord('a'):
 #                 print('key "a" pressed')  # -> 이 이벤트도 DB에 저장해보세요
+#                 print('width: {}, height : {}'.format(w*ratio, h*ratio))
+#                 toggle = 1
 
 #             elif (key & 0xFF) == 27:  # ESC 키(27?)를 누르면 break, 캠화면 닫힘
 #                 print('Closing windows')
