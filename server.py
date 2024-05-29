5c7552bbeeeffefc3dcdf812e33928c7fd2868fe
sunny7319 <sunyoung0509@naver.com>
2024-05-29 13:56:41 +0900

    민아꺼 합치고 수정

--- server.py
+++ server.py
@@ -201,34 +201,57 @@ def generate_frames():
             yield (b'--frame\r\n'
                 b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
+
+# 글로벌 변수로 설정
+toggle = 0
+
+game_data = {
+    '그림자 놀이터': {
+        'title': '그림자 놀이터',
+        'image': 'game1.png',
+        'description': '손으로 동물과 모양을 만들어 보세요!'
+    },
+    '두더지 잡기': {
+        'title': '두더지 잡기',
+        'image': 'game1.png',
+        'description': '주어진 단어와 같은 단어를 가지고 있는 두더지를 잡아보세요!'
+    },
+    '산성비': {
+        'title': '산성비',
+        'image': 'game1.png',
+        'description': '제시된 알파벳과 같은 글자를 고르는 게임입니다.'
+    }
+}
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/game/<game_name>')
 def game(game_name):
-    game_data = {
-        '그림자 놀이터': {
-            'title': '그림자 놀이터',
-            'description': '손으로 동물과 모양을 만들어 보세요!'
-        },
-        '두더지 잡기': {
-            'title': '두더지 잡기',
-            'description': '주어진 단어와 같은 단어를 가지고 있는 두더지를 잡아보세요!'
-        },
-        '산성비': {
-            'title': '산성비',
-            'description': '제시된 알파벳과 같은 글자를 고르는 게임입니다.'
-        }
-    }
+    global game_data
     if game_name in game_data:
-        return render_template('game_temp.html', game=game_data[game_name])
+        return render_template('game.html', game=game_data[game_name])
+    else:
+        return "Game not found", 404
+    
+@app.route('/game_play/<game_name>')
+def game_play(game_name):
+    global game_data
+    if game_name in game_data:
+        return render_template('game_video.html', game=game_data[game_name])
     else:
         return "Game not found", 404
 
 @app.route('/video_feed')
 def video_feed():
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+# toggle 값을 반환하는 엔드포인트 추가
+@app.route('/toggle_status')
+def toggle_status():
+    global toggle
+    return jsonify({'toggle': toggle})
 
 if __name__ == '__main__':
     app.run(debug=True)
