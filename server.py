ae2dd4a54aede1148cf2b02ae1478c5cc9eef5ea
sunny7319 <sunyoung0509@naver.com>
2024-06-03 19:32:59 +0900

    game3 합치는 중..

--- server.py
+++ server.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, Response, request, redirect, url_for, jsonify
 from login import save_user, load_users
 from game1 import check_frames, generate_frames, get_score, get_position, get_labels_positions
+from game3 import predict_image
+from PIL import Image
+import base64
+from io import BytesIO
 
 app = Flask(__name__)
 
@@ -20,12 +24,17 @@ game_data = {
         'image': 'game1.png',
         'description': '주어진 단어와 같은 단어를 가지고 있는 두더지를 잡아보세요!'
     },
-    '산성비': {
-        'title': '산성비',
+    '이미지 퀴즈쇼': {
+        'title': '이미지 퀴즈쇼',
         'image': 'game1.png',
         'description': '제시된 알파벳과 같은 글자를 고르는 게임입니다.'
     }
 }
+
+
+#########################################
+########## 로그인/회원가입 화면 ##########
+#########################################
 
 @app.route('/')
 def index():
@@ -54,9 +63,24 @@ def signup():
             return render_template('signup.html', error=error)
     return render_template('signup.html')
 
+
+#############################
+########## 메인화면 ##########
+#############################
+
 @app.route('/main')
 def main():
     return render_template('main.html')
+
+# 캠 잘 작동되는지 확인
+@app.route('/check_video')
+def check_video():
+    return Response(check_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+##################################
+########## 게임 대기화면 ##########
+##################################
 
 @app.route('/game/<game_name>')
 def game(game_name):
@@ -65,6 +89,11 @@ def game(game_name):
         return render_template('game.html', game=game_data[game_name])
     else:
         return "Game not found", 404
+
+
+#############################
+########## 게임화면 ##########
+#############################
     
 @app.route('/game_play/<game_name>')
 def game_play(game_name):
@@ -74,15 +103,15 @@ def game_play(game_name):
         return render_template('1.html', game=game_data[game_name], score=get_score(), position=position, image_info=image_info)
     elif game_name == '두더지 잡기':
         return render_template('2.html')
-    elif game_name == '산성비':
-        return render_template('3.html')
+    elif game_name == '이미지 퀴즈쇼':
+        return render_template('3.html', game=game_data[game_name])
     else:
         return "Game not found", 404
-    
-# 게임 대기화면에 표시할 캠 미리보기
-@app.route('/check_video')
-def check_video():
-    return Response(check_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+###########################
+########## game1 ##########
+###########################
 
 # 게임 화면에 들어갈 mediapipe 비디오
 @app.route('/video_feed')
@@ -114,6 +143,32 @@ def get_position_route():
 def toggle_status():
     global toggle
     return jsonify({'toggle': toggle})
+
+
+###########################
+########## game2 ##########
+###########################
+
+
+
+
+
+
+###########################
+########## game3 ##########
+###########################
+
+@app.route('/capture', methods=['POST'])
+def capture_image():
+    data = request.get_json()
+    img_data = data['image']
+    img_data = img_data.split(",")[1]
+    img = Image.open(BytesIO(base64.b64decode(img_data)))
+    predicted_class, predicted_prob, _ = predict_image(img)
+    return jsonify({'class': predicted_class, 'probability': predicted_prob})
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
