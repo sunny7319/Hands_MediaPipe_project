3f1a20e33e4ac54bba1d039c8d0b1559acdc5479
LeeMin-a <lma000424@gmail.com>
2024-06-04 21:49:12 +0900

    UI fix

--- static/js/loading.js
+++ static/js/loading.js
@@ -3,5 +3,5 @@ window.onload = function() {
     setTimeout(function() {
         document.getElementById('loading-screen').style.display = 'none';
         document.getElementById('content').style.display = 'block';
-    }, 2000);  // 2초 지연
+    });
 }
